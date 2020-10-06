bb4bfa5ce1628a9b3567badc9fd5426810aedaa5
Natalia <nataliarudomyr@gmail.com>
2020-10-06 11:59:15 +0300

    Updates

---
+++ precache-manifest.ff2fd8b26c0bdb414503dd0cdbdc0c64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2a4aa138f79cec7a75ec0b95651d2e",
+    "url": "/styledcomponents/index.html"
+  },
+  {
+    "revision": "7e1fdcc2c234a6350a87",
+    "url": "/styledcomponents/static/css/main.41462a74.chunk.css"
+  },
+  {
+    "revision": "af00ee201dcdbfb6a9ee",
+    "url": "/styledcomponents/static/js/2.28c8747f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/styledcomponents/static/js/2.28c8747f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1fdcc2c234a6350a87",
+    "url": "/styledcomponents/static/js/main.e1698218.chunk.js"
+  },
+  {
+    "revision": "fa7056f965168d6fee7d",
+    "url": "/styledcomponents/static/js/runtime-main.bff4f0d9.js"
+  }
+]);
